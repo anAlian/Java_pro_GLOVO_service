e3c38d2711e61f67bf10a32d9661b631016ef318
anAlian <absolute1246abc@gmail.com>
2023-12-13 21:36:32 +0100

    task2 is completed

--- WorkWithList/src/main/java/Task2.java
+++ WorkWithList/src/main/java/Task2.java
@@ -5,10 +5,12 @@ import java.util.List;
 public class Task2 {
     public static void main(String[] args) {
         String[] stringArray = {"str1", "str2", "str3"};
-        Integer[] intArray = {1, 3, 7, 6, 9};
+        Integer[] intArray = {1, 3, 7, 6, 9, 8};
+        Character [] charArray = {'a','d'};
 
         System.out.println(fromArrayToList(stringArray));
         System.out.println(fromArrayToList(intArray));
+        System.out.println(fromArrayToList(charArray));
 
     }
     public static <T> List<T> fromArrayToList(T[] a) {
